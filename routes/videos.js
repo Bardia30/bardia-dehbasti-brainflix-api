4513345c23cf5b2a2a030d508ef5511f0feec6e3
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-07-05 22:22:45 -0400

    added post API endpoint and logic to read and write to db

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
+
+router.use(express.json());
 
 
 router.get('/', (req, res)=>{
@@ -15,7 +18,6 @@ router.get('/', (req, res)=>{
                     id: video.id, 
                     title: video.title, 
                     channel: video.channel, 
-                    // image: `http://localhost:5050/videos/${video.image}` 
                     image: video.image
                 }
             })
@@ -32,10 +34,6 @@ router.get('/:videoId', (req, res)=>{
             res.status(401).send(err); //make sure status code is correct
         } else {
             const foundVideo = videoData.find(video => video.id === req.params.videoId );
-            // const foundVideoWithUrl = {
-            //     ...foundVideo,
-            //     image: `http://localhost:5050/videos/${foundVideo.image}`
-            // }
             if (foundVideo) {
                 res.status(201).json(foundVideo);
                 
@@ -48,6 +46,43 @@ router.get('/:videoId', (req, res)=>{
     });
 });
 
+
+router.post('/', (req, res)=>{
+    const userTitle = req.body.videoTitle;
+    const userDescription = req.body.videoDescription;
+    //TODO: check if title and description are empty or not
+
+    fs.readFile('./data/videos.json', 'utf8', (err, data)=>{
+        if (err) {
+            return res.send(err);
+        }
+        const videoData = JSON.parse(data);
+
+        const newVideo = {
+            id: uuidv4(),
+            title: userTitle,
+            channel: 'bardiCodes',
+            image: 'user-image/Upload-video-preview.jpg',
+            description: userDescription,
+            views: Math.round(Math.random() * 1000000) + 1000,
+            likes: Math.round(Math.random() * 10000),
+            duration: `${Math.round(Math.random()*12)}:${Math.round(Math.random()*5)}${Math.round(Math.random()*9)}`,
+            video: "https://project-2-api.herokuapp.com/stream",
+            timestamp: Date.now(),
+            comments: [],
+        };
+
+        videoData.push(newVideo);
+
+        fs.writeFile('./data/videos.json', JSON.stringify(videoData), (err)=>{
+            if (err){
+                return res.send(err);
+            }
+            res.status(201).send(`your new video titled "${req.body.videoTitle}" has been uploaded`);
+        })
+    })
+    // res.status(201).json(`your new video titled "${req.body.videoTitle}" has been uploaded`);
+})
 
 
 module.exports = router;
